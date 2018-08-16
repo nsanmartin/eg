e21689e9d0ee364ba911179a0556fa5aeaf9031e
nsm <smnicolas>
2018-08-16 19:27:06 -0300

    vector tests

--- cpp/containers/vector.hpp
+++ cpp/containers/vector.hpp
@@ -6,24 +6,20 @@ class vector {
     int sz;
     int capacity;
 public:
-    vector(int s) {
+    vector(int s = 0) {
         if (s < 0)
             throw std::runtime_error(std::string{
                     "negative size for vector"});
         elem = new T[sz=s];
-        capacity = sz + 1;
+        capacity = sz + 4;
     }
+
     vector(const vector& v);
     vector& operator=(const vector& v);
      
     ~vector() { delete[] elem; }
 
-    T& operator[](int i) {
-        if (i < 0 || sz <= i)
-            throw std::runtime_error(std::string{
-                    "index out of range"});
-        return elem[i];
-    }
+    T& operator[](int i);
 
     T* begin() { return elem; }
     T* end() { return elem + sz; }
@@ -33,14 +29,23 @@ public:
     int size() const { return sz;};
     int max_capacity() const { return capacity; }
     void push_back(const T&);
+    T& pop_back();
 };
 
 
 template<typename T>
 vector<T>::vector(const vector& v)
-    : elem{new T[v.sz]}, sz{v.sz}, capacity{v.sz}
+    : elem{new T[v.sz + 4]}, sz{v.sz}, capacity{v.sz + 4}
 {
     std::copy(v.begin(), v.end(), elem);
+}
+
+template<typename T>
+T& vector<T>::operator[](int i) {
+    if (i < 0 || sz <= i)
+        throw std::runtime_error(std::string{
+                "index out of range"});
+    return elem[i];
 }
 
 
@@ -54,5 +59,19 @@ void vector<T>::push_back(const T& x) {
     }
 
     elem[sz++] = x;
-    
+}
+
+
+template<typename T>
+T& vector<T>::pop_back() {
+    if (!sz)
+        throw std::runtime_error(std::string{
+                "negative size for vector"});
+    if (sz < capacity/4) {
+        T* a = new T[capacity /= 2];
+        std::copy(elem, elem + sz, a);
+        delete [] elem;
+        elem = a;
+    }
+    return elem[--sz];
 }

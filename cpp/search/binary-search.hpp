fb581ac0339b36264a5abe7ec3c281b4df5a8fa9
nsanmartin <smnicolas>
2019-04-07 20:13:26 -0300

    busquedas binarias

--- cpp/search/binary-search.hpp
+++ cpp/search/binary-search.hpp
@@ -1,20 +1,51 @@
 #include <vector>
 #include <cassert>
+#include <iterator>
+#include <iostream>
+
+template <typename I, // forward iterator
+          typename T>
+I bin_lower_bound(I f, I l, const T& v) {
+    while (f != l) {
+        auto m = std::next(f, std::distance(f, l) / 2);
+
+        if (*m < v) { f = std::next(m); }
+        else l = m;
+    }
+    return f;
+}
 
 template <typename T>
-size_t binary_search(const std::vector<T> array, const  T& key) {
-    assert (array.size() >= 0);    
+size_t binary_search_leftmost(const std::vector<T> array, const  T& key) {
+    assert (array.size() >= 1);    
     size_t low {0};
     size_t high{array.size() - 1};
 
     while (low < high - 1) {
         size_t med {low + (high - low) / 2};
-        if ( array[med] >= key) { high = med; }
+        if (array[med] >= key) { high = med; }
+        else { low = med;  }
+    }
+
+    if (array[low] >= key) { return low; }
+    return high;
+}
+
+template <typename T>
+size_t
+binary_search_decreasing(const std::vector<T> array, const  T& key) {
+    assert (array.size() > 0);    
+    size_t low {0};
+    size_t high{array.size() - 1};
+
+    while (low < high - 1) {
+        size_t med {low + (high - low) / 2};
+        if ( array[med] < key) { high = med; }
         else { low = med;  }
     }
     
-    if ( array[low] >= key) { return low; }
-    else { return high; }
+    if ( array[low] > key) { return high; }
+    else { return low; }
     
     return 0;
 }
--- cpp/search/tbinary-search.cpp
+++ cpp/search/tbinary-search.cpp
@@ -1,18 +1,71 @@
 #include <iostream>
 #include <vector>
-
+#include <iterator>
 #include "binary-search.hpp"
-int main () {
-    std::vector<int>array {0, 0, 1, 1, 2, 3, 3, 4, 8 };
+#include <random>
+#include <algorithm>
 
-    std::cout << "array: ( ";
-    for (auto& x : array) { std::cout << x << " "; }
-    std::cout << ")" << std::endl;
+template<typename I>
+void print_array(I it, const I end) {
+    using namespace std;
+    cout << "array: ( ";
+    while (it != end) {
+        cout << *it << " ";
+        it = next(it);
+    }
+    cout << ")" << endl;
+}
+
+template<typename I>
+void print_indexes(I it, const I end) {
+    using namespace std;
+    cout << "array: ( ";
+    int indx{};
+    while (it != end) {
+        cout << indx++ << " ";
+        it = next(it);
+    }
+    cout << ")" << endl;
+}
+
+
+int main () {
+    std::random_device random_device;
+
+    std::mt19937 mersenne_engine {random_device()};
+    std::uniform_int_distribution<> distribution {0, 16};
+
+    auto gen = [&distribution, &mersenne_engine]() {
+        return distribution(mersenne_engine);
+    };
+
+    auto fill_vector_with_random_numbers = [&gen](std::vector<int>& vector) {
+        std::generate(std::begin(vector), std::end(vector), gen);
+    };
+
+
+    size_t n{9};
+    std::vector<int> array(n);
+    fill_vector_with_random_numbers(array);
+    std::sort (array.begin(), array.end(), [](int x, int y) {return y < x;});
+
+    print_array(array.begin(), array.end());
+    print_indexes(array.begin(), array.end());
+    std::cout << "search-for   bin-indx   bin-val  lb-dist   ln-val"
+              << std::endl;
+    
     for (int key = -2; key < static_cast<int>(array.size()) + 2;
          key++) {
-        size_t res { binary_search(array, key) };
-    
-        std::cout << "search: " << key <<  " found: " << res
-                  << ": " << array[res] << std::endl;
+
+        size_t res { binary_search_decreasing(array, key) };
+        auto res_lb = std::lower_bound (
+            array.begin(), array.end(), key,
+            [](int x, int y) { return y < x; }
+            );
+        std::cout << "    " << key << "\t\t"
+                  << res << "\t   " << array[res] << "\t\t"
+                  << std::distance(array.begin(), res_lb)
+                  << "\t" << *res_lb
+                  << std::endl;
     }
 }

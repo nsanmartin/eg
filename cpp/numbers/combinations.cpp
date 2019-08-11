0b36b90118b9d6c1ec429b416582d9f364a1ea25
nsanmartin <smnicolas>
2019-08-11 00:15:50 -0300

    variadics

---
+++ cpp/numbers/combinations.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <cassert>
+#include <unistd.h>
+
+class Combinations {
+    int k;
+    std::vector<int> from;
+    std::string elems;
+    std::vector<int> comb;
+public:
+    Combinations(int n, int k) : k{k}, from(n), elems{}, comb{}{
+        assert(k <= n);
+        std::iota(from.begin(), from.end(), 0);
+        comb.reserve(k);
+    }
+
+    Combinations(std::string elems, int k)
+        : k{k}, from(elems.size()), elems{elems}, comb{} {
+            assert(k <= elems.size());
+            std::iota(from.begin(), from.end(), 0);
+            comb.reserve(k);
+    }
+
+    void print_combinations(int index, int tomados);
+    void print_comb() const;
+};
+
+void Combinations::print_comb() const {
+    if (elems.size() == from.size()) {
+        for (auto& index: comb)
+            std::cout << elems[index];
+        std::cout << "\n";
+        
+    } else {
+        for (auto& x: comb) std::cout << x << " ";
+        std::cout << "\n";
+    }
+}
+
+void Combinations::print_combinations(int index, int tomados) {
+    if (tomados == k) {
+        print_comb();
+    } else {
+        for (int i = index; i < from.size(); ++i) {
+            comb.push_back(from[i]);
+            print_combinations(i + 1, tomados + 1);
+            comb.pop_back();
+        }
+        
+    }
+}
+
+
+int main(int argc, char** argv) {
+    char c;
+    bool string_param{false};
+    while ((c = getopt(argc, argv, "s")) != -1) {
+        switch (c) {
+        case 's':
+            string_param = true;
+            break;
+        case '?':
+            std::cerr << "Opcion desconocida"
+                      <<  optopt;
+            return 1;
+        default:
+            abort ();
+	
+        }
+    }
+    
+    if (argc - optind != 2) {
+        std::cout << "./prog <n> <k> \n";
+    } else {
+
+        int k{std::atoi(argv[optind+1])};
+
+        if (string_param) {
+            std::string string{argv[optind]};
+            Combinations combs(string, k);
+            combs.print_combinations(0, 0);
+        } else {
+            int n{std::atoi(argv[optind])};
+            Combinations combs(n, k);
+            combs.print_combinations(0, 0);
+        }
+    }
+
+}
---
+++ cpp/variadics/variadic-tuple.cpp
@@ -0,0 +1,59 @@
+// so: questions/27941661/generating-one-class-member-per-variadic-template-argument
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <functional>
+
+template<class T1, class T2> struct AreSameType { static const bool value = false; };
+template<class T> struct AreSameType<T,T> { static const bool value = true; };
+
+
+template<typename... Types>
+class dataframe {
+public:
+    typedef std::tuple<std::vector<Types>...> vtype;
+    vtype vectors;
+
+    template<int N, typename T>
+    struct VectorOfType:
+	AreSameType<T,typename std::tuple_element<N, vtype>::type::value_type>
+    {};
+
+    template<int N, class T, class Tuple, bool Match = false>
+    struct MatchingField {
+	static std::vector<T>& get(Tuple& tp) {
+	    return MatchingField<N+1, T, Tuple, VectorOfType<N+1, T>::value>::get(tp);
+	}
+    };
+
+
+    template<int N, class T, class Tuple>
+    struct MatchingField<N, T, Tuple, true> {
+	static std::vector<T>& get(Tuple& tp) { return std::get<N>(tp); }
+    };
+    
+    template<typename T>
+    std::vector<T>& access() {
+	return MatchingField<0, T, vtype, VectorOfType<0, T>::value>::get(vectors);
+    }
+};
+				  
+
+int main () {
+    int twelf = 12.5;
+    typedef std::reference_wrapper<int> rint;
+
+    dataframe<float, rint> mc;
+    
+    std::vector<rint>& i = mc.access<rint>();
+
+    i.push_back(twelf);
+
+    mc.access<float>().push_back(10.5);
+
+    std::cout << "floats: " << mc.access<float>()[0] << '\n'
+    	      << "ints: " << mc.access<rint>()[0] << '\n';
+
+
+    dataframe<int, std::string, std::string, int, float> df;
+}

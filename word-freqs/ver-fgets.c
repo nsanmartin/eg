7a31e04185cb6b5d881db7b5f98868d9df585656
nico <smnicolas@gmail.com>
2022-09-04 21:36:47 -0300

    java spring

--- word-freqs/ver-fgets.c
+++ word-freqs/ver-fgets.c
@@ -11,14 +11,15 @@ char * skip_space_or_null(char* beg, char* end) ;
 #define SPACE 1
 
 typedef struct {
-    char* newline;
-    char* last[2];
-} Line;
-
-typedef struct {
     char* beg;
     char* end;
 } Range;
+
+typedef struct {
+    Range range;
+    bool end_of_line;
+} Line;
+
 
 int range_len(Range r) { return r.end - r.beg; }
 
@@ -29,6 +30,9 @@ bool range_is_empty(Range r) { return r.end <= r.beg || *r.beg == '\n'; }
 bool range_has_next(Range r) { return r.end > r.beg && *r.beg != '\n'; }
 bool subrange_has_word(Range r, char* end) { return r.end < end && (isspace(*r.end) || *r.end == '\0'); }
 bool range_is_eol(Range r) { return r.beg < r.end && *r.beg == '\n'; }
+bool end_of_line(Line ln) { 
+    return ln.end_of_line ||  (ln.range.beg < ln.range.end && *ln.range.beg == '\n'); 
+}
 
 char* first_not_space(Range r) {
     char* rv = r.beg;
@@ -36,7 +40,7 @@ char* first_not_space(Range r) {
     return rv;
 }
 
-Range get_next_word(Range r) {
+Range range_get_next_word(Range r) {
     while (range_has_next(r)  && (isspace(*r.beg) || *r.beg == '\0')) { ++r.beg; }
     r.end = first_not_space(r);
     return r;
@@ -51,37 +55,48 @@ int copy_range(Range r, char* dest) {
     return size;
 }
 
+//TODO: do a WordStream reader such as the read version
+
 int main(void)
 {
     char buf[BUFFER_SIZE];
 
     int outer = 0;
     int inner = 0;
-    Range visible = { .beg = buf, .end = buf + BUFFER_SIZE };
 
-    for ( ; fgets(visible.beg, range_len(visible), stdin) != NULL; ) {
-        visible.beg = buf;
+    Line ln = { .range = { .beg = buf, .end = buf + BUFFER_SIZE}, .end_of_line = false };
+
+    for ( ; fgets(ln.range.beg, range_len(ln.range), stdin) != NULL; ) {
+
+        ln.range.beg = buf;
+        ln.end_of_line = false;
         Range w = {.beg = 0, .end = 0};
-        while (range_has_next(visible)) {
-            w = get_next_word(visible);
-            visible.beg = w.end;
-            if(subrange_has_word(w, visible.end)) {
+
+        while (!ln.end_of_line && range_has_next(ln.range)) {
+            w = range_get_next_word(ln.range);
+
+
+            if(subrange_has_word(w, ln.range.end)) {
                 if (range_has_next(w)) {
+                    ln.end_of_line = *w.end == '\n';
+        
                     *w.end = '\0';
-                    //printf("%s\n", w.beg);
-                    printf("%.*s\n", (int)(w.end - w.beg), w.beg); 
+                    printf("%s\n", w.beg); 
                 }
             }
+
+            ln.range.beg = w.end;
+
         }
 
         int ncopied = 0;
-        if (!range_is_eol(visible) && range_has_next(w)) {
+        if (!end_of_line(ln) && range_has_next(w)) {
             ncopied = copy_range(w, buf);
-            visible.beg = buf + ncopied;
-            visible.end = buf + BUFFER_SIZE - ncopied;
+            ln.range.beg = buf + ncopied;
+            ln.range.end = buf + BUFFER_SIZE - ncopied;
         }
 
-        visible.beg = buf + ncopied;
-        visible.end = buf + BUFFER_SIZE - ncopied;
+        ln.range.beg = buf + ncopied;
+        ln.range.end = buf + BUFFER_SIZE - ncopied;
     }
 }

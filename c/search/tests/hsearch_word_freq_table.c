9afae9289de2361a84704b7aca40dcf52971358a
nico <smnicolas@gmail.com>
2022-12-13 12:22:35 -0300

    Fix strdup ownership

--- c/search/tests/hsearch_word_freq_table.c
+++ c/search/tests/hsearch_word_freq_table.c
@@ -25,14 +25,21 @@ int cmp_by_freq(const void* p, const void* q) {
 }
 
 int table_init(VecEntry* v, int initial_capacity) {
+    Entry* entries = malloc(initial_capacity * sizeof(Entry));
+    if (!entries) {
+        fprintf(stderr, "table_init: Not memory\n");
+        return -1;
+    }
     *v = (VecEntry){
         .sz = 0,
         .cap = initial_capacity,
-        .entries = malloc(initial_capacity * sizeof(Entry))
+        .entries = entries
     };
-    if (!v->entries) { return -1; }
     int initial_htable_size = 1 + ((v->cap * 5) / 4);
     int error = hcreate(initial_htable_size) == 0;
+    if (error) {
+        fprintf(stderr, "table_init: hcreate error\n");
+    }
     return error;
 }
 
@@ -48,24 +55,46 @@ int table_duplicate(VecEntry* table) {
     table->cap *= 2;
     table->entries = realloc(table->entries, table->cap * sizeof(Entry));
     if (!table->entries) {
+        fprintf(stderr, "table_duplicate: mem error\n");
         return -1;
     }
     hdestroy();
     int error = hcreate(1+((table->cap * 5) / 4)) == 0;
-    if (error)
+    if (error) {
+        fprintf(stderr, "table_duplicate: hcreate error\n");
         return error;
+    }
 
     ENTRY e;
     for (intptr_t i = 0; i < table->sz; ++i) {
-        e =  (ENTRY){ .key=table_get(table, i)->key, .data=(void*)i} ;
-        hsearch(e, ENTER);
+        Entry* entry = table_get(table, i);
+        if (!entry|| !entry->key) {
+            fprintf(stderr, "table_duplicate: error entry not found\n");
+            return -1;
+        }
+        char* k = strdup(entry->key);
+        if (!k) {
+            fprintf(stderr, "table_duplicate: mem error\n");
+            return -1;
+        }
+        e =  (ENTRY){ .key=k, .data=(void*)i} ;
+        ENTRY* ep = hsearch(e, ENTER);
+        if (!ep) {
+            fprintf(stderr, "table_duplicate: hsearch error\n");
+            return -1;
+        }
     }
     return 0;
 }
 
 int table_add_symbol(VecEntry* table, char* word) {
     intptr_t count = -1;
-    ENTRY e = { .key = word, .data = (void*)count };
+    char* w1 = strdup(word);
+    if (!w1) {
+        fprintf(stderr, "table_add_symbol: mem error\n");
+        return -1;
+    }
+    ENTRY e = { .key = w1, .data = (void*)count };
     ENTRY* ep = hsearch(e, ENTER);
     if (!ep) {
         // unexpected, table size should be grater than htable size + 1
@@ -77,9 +106,17 @@ int table_add_symbol(VecEntry* table, char* word) {
         // new entry
         if (table->sz >= table->cap) {
             int error = table_duplicate(table);
-            if (error) 
+            if (error) {
+                fprintf(stderr, "table_add_symbol: error duplicating\n");
                 return error;
+            }
 
+            char* w2 = strdup(word);
+            if (!w2) {
+                fprintf(stderr, "table_add_symbol: mem error\n");
+                return -1;
+            }
+            e.key = w2;
             ep = hsearch(e, ENTER);
             if (!ep) {
                 // unexpected, table size should be grater than htable size + 1
@@ -88,23 +125,21 @@ int table_add_symbol(VecEntry* table, char* word) {
             }
         }
         index = table->sz++;
-        //ep = hsearch(e, ENTER);
-        //if (!ep) {
-        //    // unexpected, table size should be grater than htable size + 1
-        //    fprintf(stderr, "table is full but was just duplicated :/!\n");
-        //    return -1;
-        //}
 
         if (!ep) {
                 fprintf(stderr, "WTH??\n");
                 exit(1);
         }
         ep->data = (void*)index;
-        ep->key = strdup(word);
-        table->entries[index] = (Entry){ .key = ep->key, .count = 1 };
-        //table->entries[index] = (Entry){ .key = strdup(word), .count = 1 };
+        char* w3 = strdup(word);
+        if (!w3) {
+            fprintf(stderr, "table_add_symbol: mem error\n");
+            return -1;
+        }
+        table->entries[index] = (Entry){ .key = w3, .count = 1 };
     } else {
         ++table->entries[index].count;
+        free(w1);
     }
     return 0;
 }
@@ -121,7 +156,6 @@ int count_words (VecEntry* symbols, char* filename) {
          while ((read = getline(&line, &len, fp)) != -1) {
              char* token = strtok(line, delimiters);
              while(token) {
-                 //printf("`%s'\n", token);
                  int error = table_add_symbol(symbols, token);
                  if (error) {
                      return error;
@@ -156,12 +190,14 @@ int main (int argc, char ** argv) {
           }
      }
 
+     hdestroy();
      //qsort(symbols.entries, symbols.sz, sizeof(Entry), cmp_by_freq);
 
      for (int i = 0; i < symbols.sz; ++i) {
          Entry* e = table_get(&symbols, i);
          if (e) {
              printf("%s: %d\n", e->key, e->count);
+             free(e->key);
          }
      }
 
